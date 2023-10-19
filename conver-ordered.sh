abd7a2e5f17df73f1f4e9954347f3ac0b42b8e10
Manoel <mancordosa@gmail.com>
2023-10-19 12:39:40 -0300

    Quoting

--- conver-ordered.sh
+++ conver-ordered.sh
@@ -12,27 +12,27 @@ done < <(printf '%s\0' "${sorthelper[@]}" | sort -z)
 
 for file in "${sorted[@]}"; do
 	if [[ $file == *.png ]]; then
-		cwebp -short -metadata all $file -o $(basename $file .png ).webp &
+		cwebp -short -metadata all "$file" -o "$(basename $file .png )".webp &
 		wait
 		rm $file
 	elif [[ $file == *.jpg ]]; then
-		cwebp -short -metadata all $file -o $(basename $file .jpg).webp &
+		cwebp -short -metadata all "$file" -o "$(basename $file .jpg)".webp &
 		wait
 		rm $file
 	elif [[ $file == *.jpg ]]; then
-		cwebp -short -metadata all $file -o $(basename $file .jpeg).webp &
+		cwebp -short -metadata all "$file" -o "$(basename $file .jpeg)".webp &
 		wait
-		rm $file
+		rm "$file"
 	elif [[ $file == *.gif ]]; then
-		gif2webp -mixed -mt -metadata all $file -o $(basename $file .gif).webp &
+		gif2webp -mixed -mt -metadata all $file -o "$(basename $file .gif)".webp &
 		wait
 		rm $file
 	elif [[ $file == *.mp4 ]]; then
-		cp $file $(basename $file .mp4)1.mp4 &
+		cp "$file" "$(basename "$file" .mp4)"1.mp4 &
 		wait
 		rm $file
 	elif [[ $file == *.webm ]]; then
-		cp $file $(basename $file .webm)1.webm &
+		cp $file "$(basename $file .webm)"1.webm &
 		wait
 		rm $file
 	fi
