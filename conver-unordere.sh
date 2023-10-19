257094e1082be3923fab377fba027c10722e09b6
Manoel <mancordosa@gmail.com>
2023-10-19 15:35:06 -0300

    Adding new conversor

---
+++ conver-unordere.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+fd . './' -e jpg -e png -e jpeg --exec cwebp {} -o "{.}".webp;
+fd . './' -e jpg -e png -e jpeg --exec-batch rm {};
+# Need to find the right config, because the webp gifs are laging _a lot_
+fd . './' -e gif --exec gif2webp -mt -metadata all -m 6 -q 100 {} -o "{.}".webp;
+fd . './' -e jpg -e png -e jpeg -e gif --exec-batch rm {};
+
+# fd . './' -e jpg -e png -e jpeg --exec-batch rm {};
+
+fd . './' -e mp4 --exec ffmpeg -i {} -c:v libaom-av1 -strict -2 "{.}".mkv;
+fd . './' -e mp4 --exec-batch rm {};
