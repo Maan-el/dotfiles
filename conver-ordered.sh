386d0c8f23302fb472e95ba30c3ebbfeba822504
Manoel <mancordosa@gmail.com>
2023-10-20 13:09:43 -0300

    Print resumido removido

--- conver-ordered.sh
+++ conver-ordered.sh
@@ -12,21 +12,17 @@ done < <(printf '%s\0' "${sorthelper[@]}" | sort -z)
 
 for file in "${sorted[@]}"; do
 	if [[ $file == *.png ]]; then
-		cwebp -short -metadata all "$file" -o "$(basename $file .png )".webp &
-		wait
-		rm $file
+		cwebp -metadata all "$file" -o "$(basename $file .png )".webp &
+		wait;
 	elif [[ $file == *.jpg ]]; then
-		cwebp -short -metadata all "$file" -o "$(basename $file .jpg)".webp &
-		wait
-		rm $file
-	elif [[ $file == *.jpg ]]; then
-		cwebp -short -metadata all "$file" -o "$(basename $file .jpeg)".webp &
-		wait
-		rm "$file"
+		cwebp -metadata all "$file" -o "$(basename $file .jpg)".webp &
+		wait;
+	elif [[ $file == *.jpeg ]]; then
+		cwebp -metadata all "$file" -o "$(basename $file .jpeg)".webp &
+		wait;
 	elif [[ $file == *.gif ]]; then
-		gif2webp -mixed -mt -metadata all $file -o "$(basename $file .gif)".webp &
-		wait
-		rm $file
+		gif2webp -mixed -mt -metadata all "$file" -o "$(basename $file .gif)".webp &
+		wait;
 	elif [[ $file == *.mp4 ]]; then
 		ffmpeg -i "$file" -c:v libaom-av1 -strict -2 "$(basename $file .mp4)".mkv &
 		wait;
