cf8b84d93d761d47339e92d5f2569649eee5d80b
Manoel <mancordosa@gmail.com>
2023-10-20 13:12:27 -0300

    Informações das imagens originais agor são transmitidas para a nova imagem

--- conver-unordere.sh
+++ conver-unordere.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-fd . './' -e jpg -e png -e jpeg --exec cwebp {} -o "{.}".webp;
+fd . './' -e jpg -e png -e jpeg --exec cwebp -metadata all {} -o "{.}".webp;
 fd . './' -e jpg -e png -e jpeg --exec-batch rm {};
 # Need to find the right config, because the webp gifs are laging _a lot_
 fd . './' -e gif --exec gif2webp -mt -metadata all -m 6 -q 100 {} -o "{.}".webp;
